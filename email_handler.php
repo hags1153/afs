d6685809b85b33434ec895bcc8e12caecb13ac5a
AZRH <rhags1134@gmail.com>
2025-01-18 12:49:39 -0500

    Add email_handler.php

---
+++ email_handler.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "sean@atlantisfireandsecurity.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    $emailBody = "
+        <html>
+        <body>
+            <h2>New Contact Form Submission</h2>
+            <p><strong>Name:</strong> $name</p>
+            <p><strong>Email:</strong> $email</p>
+            <p><strong>Message:</strong></p>
+            <p>$message</p>
+        </body>
+        </html>
+    ";
+
+    if (mail($to, $subject, $emailBody, $headers)) {
+        echo "Thank you for contacting us, $name. We'll get back to you shortly.";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
